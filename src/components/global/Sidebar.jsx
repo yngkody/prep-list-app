d5a79e78ec564554d5b216873fa22854eb177776
Binghi <binghi@Binghis-MBP.lan>
2025-08-24 22:27:47 -0400

    Add Data section and fix sidebar styling

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -3,19 +3,12 @@ import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
-import { tokens } from "../../theme"; // import tokens once
+import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined"
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
@@ -23,11 +16,13 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
   return (
     <MenuItem
       active={selected === title}
-      style={{ color: colors.grey[100] }}
+      style={{ color: colors.grey[100], overflow: "visible" }}
       onClick={() => setSelected(title)}
       icon={icon}
     >
-      <Typography>{title}</Typography>
+      <Box sx={{ whiteSpace: "normal", wordBreak: "break-word" }}>
+        <Typography>{title}</Typography>
+      </Box>
       <Link to={to} />
     </MenuItem>
   );
@@ -42,14 +37,21 @@ const Sidebar = () => {
   return (
     <Box
       sx={{
+        height: "100vh",
+        borderRadius: "20px", // round corners
+        overflow: "hidden",
         "& .pro-sidebar-inner": {
-          background: `${colors.grey[700]} !important`,
+          background: colors.grey[700],
+          height: "100%", // extend to bottom
+          borderRadius: "20px",
         },
         "& .pro-icon-wrapper": {
           backgroundColor: "transparent !important",
         },
         "& .pro-inner-item": {
           padding: "5px 35px 5px 20px !important",
+          whiteSpace: "normal", // wrap text
+          wordBreak: "break-word",
         },
         "& .pro-inner-item:hover": {
           color: "#868dfb !important",
@@ -71,21 +73,14 @@ const Sidebar = () => {
             }}
           >
             {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                ml="15px"
-              >
-               <Typography
-              variant="h2"
-              color={colors.grey[100]}
-              sx={{ fontFamily: "'Praise', cursive" }}
-            >
-              Preppery
-            </Typography>
-
-
+              <Box display="flex" justifyContent="space-between" alignItems="center" ml="15px">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  sx={{ fontFamily: "'Praise', cursive" }}
+                >
+                  Preppery
+                </Typography>
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
                 </IconButton>
@@ -96,14 +91,6 @@ const Sidebar = () => {
           {!isCollapsed && (
             <Box mb="25px">
               <Box textAlign="center">
-                <Typography
-                  variant="h2"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  Michael Scott Catering
-                </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   Admin Portal
                 </Typography>
@@ -112,111 +99,29 @@ const Sidebar = () => {
           )}
 
           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            <Item
-              title="Dashboard"
-              to="/"
-              icon={<HomeOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
+            <Item title="Dashboard" to="/" icon={<HomeOutlinedIcon />} selected={selected} setSelected={setSelected} />
 
-           <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
+            <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
               Quick Start
             </Typography>
-
-            <Item
-              title="Stations"
-              to="/prep-list"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-            <Item
-              title="Recipes/Instructions"
-              to="/prep-list"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-            <Item
-              title="Notifcation Center"
-              to="/prep-list"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Events
-            </Typography>
-            <Item
-              title="Prep List"
-              to="/prep-list"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Workflows"
-              to="/workflows"
-              icon={<ReceiptOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Labels & Printing"
-              to="/labels-printig"
-              icon={<ReceiptOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
+            <Item title="View Short List" to="/short-list" icon={<ContactsOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="View Progess by Station" to="/station-progress" icon={<ContactsOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Edit Prep List" to="/prep-list" icon={<ContactsOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <Item title="Upload New Prep List" to="/new-prep-list" icon={<ContactsOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
               Data
             </Typography>
             <Item
-              title="Reports & Analytics"
-              to="/form"
-              icon={<PersonOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Portal
-            </Typography>
-            <Item
-              title="Support & FAQ"
-              to="/bar"
-              icon={<BarChartOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Settings & Users"
-              to="/pie"
+              title="Reports and Analytics"
+              to="/reports-analytics"
               icon={<PieChartOutlineOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
             />
+            <Typography variant="h6" color={colors.grey[300]} sx={{ m: "15px 0 5px 20px" }}>
+              Admin Portal
+            </Typography>
+            <Item title="Manage" to="/bar" icon={<BarChartOutlinedIcon />} selected={selected} setSelected={setSelected} />
           </Box>
         </Menu>
       </ProSidebar>
